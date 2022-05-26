fb00c685914dde48a2bd2e5c326f59c9dc9a06a8
Matheus Amorim <mtsgamorim@gmail.com>
2022-05-26 12:34:49 -0300

    Telas prontas para desenvolvimento

--- src/components/Habitos.js
+++ src/components/Habitos.js
@@ -1,5 +1,14 @@
+import styled from "styled-components"
+import axios from "axios"
+
+import Topo from "./Topo"
+import Menu from "./Menu"
+
 export default function Habitos(){
     return(
-        <h1>Eu sou o habitos</h1>
+        <>
+            <Topo/>
+            <Menu/>
+        </>
     )
 }
