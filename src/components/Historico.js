f9055a00c9dd3b72c9be7e10411c6f0ea60b071d
Matheus Amorim <mtsgamorim@gmail.com>
2022-05-25 21:09:50 -0300

    historico pronto

--- src/components/Historico.js
+++ src/components/Historico.js
@@ -1,5 +1,34 @@
+import Topo from "./Topo"
+import Menu from "./Menu"
+import styled from "styled-components"
+
 export default function Historico(){
     return(
-        <h1>Eu sou o historico</h1>
+        <>
+            <Topo />
+            <Container>
+                <h1>Histórico</h1>
+                <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+            </Container>
+            <Menu />
+        </>
     )
 }
+
+const Container = styled.div`
+    width: 85%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 120px;
+    h1 {
+        color:#126BA5;
+        font-family: 'Lexend Deca';
+        font-weight: 400;
+        font-size: 23px;
+    }
+    p {
+        font-family: 'Lexend Deca';
+        font-weight: 400;
+        font-size: 18px;
+    }
+`;
